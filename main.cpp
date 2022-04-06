07cd5182318d110d9e1c48a385e6d56cb0b6485a
ziadliwa001 <92695464+ziadliwa001@users.noreply.github.com>
2022-04-06 23:40:52 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,86 +1,129 @@
+// FCI – Programming 1 – 2022 - Assignment 3
+// Program Name: xxxxxx.cpp
+// Last Modification Date: 4/3/2022
+// Author1 and ID and Group: 20210143
+// Teaching Assistant: Hager Ali
+// Purpose:..........
 #include <iostream>
-#include <fstream>
-#include <cstring>
-#include <cmath>
+#include <bits/stdc++.h>
 #include "bmplib.cpp"
-using namespace std;
 unsigned char image[SIZE][SIZE];
-unsigned char image2[SIZE][SIZE];
-unsigned char newImage[SIZE][SIZE];
-void loadImage();
-void loadImage2();
-void saveImage();
-void merge();
-void lightenDarken();
-
-
-int main() {
-    int choice;
-    char  imageFileName1[100];
-    cout << "Ahlan ya user ya habibi " << endl;
-    cout << "Please enter file name of the image to process: " ;
-    cin >> imageFileName1;
-    strcat(imageFileName1,".bmp");
-    readGSBMP(imageFileName1,image);
-    cout <<"Please select a filter to apply or 0 to exit" << endl;
-    cout << "3- merge filter \n6- Darken and Lighten image" << endl;
-    cin >> choice;
-    if(choice == 3){
-        loadImage2();
-        merge();
+void loadImage ();
+void saveImage ();
+void doBlackandWhiteforImage ();
+void doMirrorupforImage ();
+void doMirrorleftforImage ();
+void doMirrorrightforImage ();
+void doMirrordownforImage();
+void Mirror();
+int main()
+{
+    char Option ;
+    cout << " Ahlan ya user ya habibi \uF04A " << endl;
+    loadImage();
+    cout << "Please select a filter to apply or 0 to exit:\n1- Black & White Filter\na- Mirror 1/2 Image";
+    cin >> Option ;
+    if (Option=='1')
+    {
+        doBlackandWhiteforImage();
         saveImage();
     }
-    else if(choice == 6){
-        lightenDarken();
+    else if (Option=='a')
+    {
+        Mirror();
         saveImage();
-
     }
+    else if (Option=='0')
+        return 0;
+    else
+        cout << "Invalid input " << endl;
 }
 
-void loadImage2(){
-    char  imageFileName2[100];
-    cout << "Enter the second source file name: ";
-    cin >> imageFileName2;
-    strcat(imageFileName2,".bmp");
-    readGSBMP(imageFileName2,image2);
+void loadImage () {
+    char imageFileName[300];
+    // Get gray scale image file name
+    cout << "Enter the source image file name: ";
+    cin >> imageFileName;
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    readGSBMP(imageFileName, image);
 }
-void saveImage(){
-    char imageFileName[100];
+void saveImage () {
+    char imageFileName[300];
+    // Get gray scale image target file name
     cout << "Enter the target image file name: ";
     cin >> imageFileName;
-    strcat(imageFileName,".bmp");
-    writeGSBMP(imageFileName,newImage);
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    writeGSBMP(imageFileName, image);
 }
-void merge(){
-    for(int i = 0; i < SIZE; i++) {
+void doBlackandWhiteforImage() {
+    long avg = 0 ;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j< SIZE; j++){
+            avg+=image[i][j];
+        }
+    }
+    avg/=(SIZE*SIZE);
+    for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
 
-            newImage[i][j] = (image[i][j] + image2[i][j])/2;
-
+            if (image[i][j] > avg)
+                image[i][j] = 255;
+            else
+                image[i][j] = 0;
         }
     }
 }
-void lightenDarken(){
-    int ch;
-    cout << "Do you want to darken or lighten the image ?" << endl;
-    cout << "1-Darken\n2-Lighten" << endl;
-    cin >> ch;
-    if(ch == 1){
-        for(int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-
-                newImage[i][j] = image[i][j]/2;
-
-            }
+void  doMirrorupforImage ()
+{
+    for (int i = SIZE; i >=0 ; i--)
+    {
+        for (int j = 0; j <SIZE ; j++) {
+            image[i][j]=image[SIZE-i][j];
         }
     }
-    else if(ch == 2){
-        for(int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                newImage[i][j] = 200;
-                newImage[i][j] = (newImage[i][j]+image[i][j])/2;
-            }
-
+}
+void doMirrorleftforImage ()
+{
+    for (int i = SIZE; i >=0 ; i--)
+    {
+        for (int j = 0; j <SIZE ; j++)
+        {
+            image[i][j]=image[i][SIZE-j];
         }
     }
+}
+void doMirrorrightforImage ()
+{
+    for (int i = 0; i <SIZE; i++)
+    {
+        for (int j = SIZE; j >=0 ; j--)
+        {
+            image[i][j]=image[i][SIZE-j];
+        }
+    }
+}
+void doMirrordownforImage()
+{
+    for (int i = 0; i <SIZE; i++) {
+        for (int j = SIZE; j >= 0; j--) {
+            image[i][j] = image[SIZE - i][j];
+        }
+    }
+}
+void Mirror(){
+    int Mirror_opt;
+    cout << " Enter an option for Mirror: 1(upper),2(down),3(right),4(left)" << endl;
+    cin >> Mirror_opt;
+    if (Mirror_opt==1)
+        doMirrorupforImage();
+    else if (Mirror_opt==2)
+        doMirrordownforImage();
+    else if (Mirror_opt==3)
+        doMirrorrightforImage();
+    else if (Mirror_opt==4)
+        doMirrorleftforImage();
+    else
+        cout << " Invalid input " << endl;
 }
